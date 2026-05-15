e2d9939d4b5105bec6898b89e0f4c19d5d0f4829
HemantKushwah14 <153351115+HemantKushwah14@users.noreply.github.com>
2026-05-15 15:56:05 +0530

    Refactor CORS middleware with specific origins
    Updated CORS configuration to specify allowed origins.

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,16 @@ const io = connectToSocket(server);
 
 
 app.set("port", (process.env.PORT || 8000))
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://your-frontend-name.onrender.com"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
